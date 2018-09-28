e4e6c17c2720696ec80ba6d62e4a7b80b10d2658
George Rossakis <george@MacBook-Pro-3.local>
2018-09-27 21:45:59 -0400

    deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var logger = require("morgan");
 var path = require("path");
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -22,7 +22,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/VGarticles");
+// mongoose.connect("mongodb://localhost/VGarticles");
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/VGarticles";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // app.get("/scrape", function(req, res){
 //     axios.get("https://www.gamespot.com/news/").then(function(response) {
